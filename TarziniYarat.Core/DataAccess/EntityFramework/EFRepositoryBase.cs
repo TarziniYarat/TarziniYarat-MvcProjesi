7db2bd31f33b93f761e3f466fe135c70ea72bbf3
ozlemaslan <aslan1997.ozlem@gmail.com>
2020-06-04 00:11:18 +0300

    core tablosu

---
+++ TarziniYarat.Core/DataAccess/EntityFramework/EFRepositoryBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TarziniYarat.Core.Model;
+
+namespace TarziniYarat.Core.DataAccess.EntityFramework
+{
+    public class EFRepositoryBase<TEntity, TContext> : IRepository<TEntity>
+        where TContext : DbContext, new()
+        where TEntity : BaseEntity
+    {
+        TContext db;
+        DbSet<TEntity> entities;
+        public EFRepositoryBase()
+        {
+            db = SingletonContext<TContext>.Context;
+            entities = db.Set<TEntity>();
+        }
+        public int Add(TEntity entity)
+        {
+            entities.Add(entity);
+            return db.SaveChanges();
+        }
+
+        public int Delete(TEntity entity)
+        {
+            entities.Remove(entity);
+            return db.SaveChanges();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            return entities.SingleOrDefault(filter);
+        }
+
+        public ICollection<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            if (filter != null)
+            {
+                return entities.Where(filter).ToList();
+            }
+            else
+            {
+                return entities.ToList();
+            }
+        }
+
+        public int Update(TEntity entity)
+        {
+            db.Entry(entity).State = EntityState.Modified;
+            return db.SaveChanges();
+        }
+    }
+}
---
+++ TarziniYarat.Core/DataAccess/EntityFramework/SingletonContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarziniYarat.Core.DataAccess.EntityFramework
+{
+    class SingletonContext<TContext> where TContext : new()
+    {
+        private static TContext _context;
+
+        internal static TContext Context
+        {
+            get
+            {
+                if (_context == null)
+                {
+                    _context = new TContext();
+                }
+                return _context;
+            }
+
+        }
+    }
+}
