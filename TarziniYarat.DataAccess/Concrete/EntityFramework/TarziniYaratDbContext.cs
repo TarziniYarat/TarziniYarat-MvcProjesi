33a70f3910cc609ec73eda0ad5beac55637d7131
Kubra Seyrek <kubraseyrek95@gmail.com>
2020-06-04 16:03:59 +0300

    dbcontext

--- TarziniYarat.DataAccess/Concrete/EntityFramework/TarziniYaratDbContext.cs
+++ TarziniYarat.DataAccess/Concrete/EntityFramework/TarziniYaratDbContext.cs
@@ -19,8 +19,16 @@ namespace TarziniYarat.DataAccess.Concrete.EntityFramework
 
         public DbSet<Role> Roles { get; set; }
         public DbSet<Person> Persons { get; set; }
-        public DbSet<Like> Users { get; set; }
-
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category>  Categories { get; set; }
+        public DbSet<Combine> Combines { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<PersonDetails> PersonDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Shipper> Shippers { get; set; }
 
 
 
