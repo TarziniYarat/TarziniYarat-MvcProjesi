0831930d224c0eb74b1cd73ffcca727a47743fe4
Kubra Seyrek <kubraseyrek95@gmail.com>
2020-06-04 15:10:32 +0300

    son işlemler

--- TarziniYarat.DataAccess/Concrete/EntityFramework/TarziniYaratDbContext.cs
+++ TarziniYarat.DataAccess/Concrete/EntityFramework/TarziniYaratDbContext.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TarziniYarat.Model;
 
 namespace TarziniYarat.DataAccess.Concrete.EntityFramework
 {
-    class TarziniYaratDbContext
+   public class TarziniYaratDbContext :DbContext
     {
+        public TarziniYaratDbContext()
+           : base("name=TarziniYaratConnStr")
+        {
+
+        }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Like> Users { get; set; }
+
+
+
+
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+
+
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+        }
     }
 }
