c72aee84215f91944969774b28b71e25fe78818b
Tuğba ÖZMEN <tugbaozmen123@gmail.com>
2020-06-05 19:34:23 +0300

    Site bölümü

---
+++ TarziniYarat.UI.MVC/Controllers/SitesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TarziniYarat.UI.MVC.Controllers
+{
+    public class SitesController : Controller
+    {
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        public ActionResult Shop()
+        {
+            return View();
+        }
+        public ActionResult Combine()
+        {
+            return View();
+        }
+        public ActionResult CreateCombine()
+        {
+            return View();
+        }
+        public ActionResult ViewCombine()
+        {
+            return View();
+        }
+        public ActionResult AllViewCombine()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
