c5f0e4b5133d2dd5b0f7a30b76b5367a8575fe0c
Tuğba ÖZMEN <65816851+tugbaozmen@users.noreply.github.com>
2020-06-07 03:24:13 +0300

    Kategori işlemleri yapıldı

--- TarziniYarat.UI.MVC/Areas/Admin/Controllers/AdminProcessController.cs
+++ TarziniYarat.UI.MVC/Areas/Admin/Controllers/AdminProcessController.cs
@@ -23,9 +23,132 @@ namespace TarziniYarat.UI.MVC.Areas.Admin.Controllers
             _personService = personService;
             _shipperService = shipperService;
         }
+
+
+        //while creating a new product,we need to add product's brand which was created before.
         public ActionResult ProductList()
         {
             return View();
+        }
+
+        public JsonResult DeleteProduct()
+        {
+            return Json(JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AddProduct()
+        {
+            GetBodyFromEnum();
+            GetColorFromEnum();
+            GetAllBrands();
+            GetAllCategories();
+            return View();
+        }
+
+        public ActionResult AddProduct(Product p)
+        {
+            GetBodyFromEnum();
+            GetColorFromEnum();
+            GetAllBrands();
+            GetAllCategories();
+
+            if (p.Description != null && p.ProductTitle != null) //buraya bak
+            {
+                _productService.Add(p);
+            }
+            return View();
+        }
+
+        public JsonResult DeletePerson()
+        {
+            return Json(JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult PersonList()
+        {
+            List<Person> personList = _personService.GetAll();
+
+            return View(personList);
+        }
+
+        public JsonResult AddPerson()
+        {
+            return Json(JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ActivatePerson(int personID)
+        {
+            Person person = _personService.GetByID(personID);
+            person.IsActive = true;
+            _personService.Update(person);
+            return Json("ok", JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult BrandList()
+        {
+            return View(_brandService.GetAll());
+        }
+
+        public ActionResult AddBrand()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddBrand(Brand brand)
+        {
+            _brandService.Add(brand);
+            return RedirectToAction("BrandList");
+        }
+
+        [HttpPost]
+        public JsonResult UpdateBrand(Brand brd)
+        {
+            try
+            {
+                Brand brand = _brandService.GetByID(brd.BrandID);
+                brand.CompanyName = brd.CompanyName;
+                _brandService.Update(brand);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GetBrand(int id)
+        {
+            Brand brand = _brandService.GetByID(id);
+
+            return Json(brand, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        public JsonResult DeleteBrand(int id)
+        {
+            try
+            {
+                _brandService.Delete(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return Json("ok", JsonRequestBehavior.AllowGet);
+        }
+
+        private void GetAllBrands()
+        {
+            List<SelectListItem> brands = new List<SelectListItem>();
+            foreach (Brand item in _brandService.GetAll())
+            {
+                brands.Add(new SelectListItem { Text = item.CompanyName, Value = item.BrandID.ToString() });
+            }
+            ViewBag.Brands = brands;
         }
 
 
@@ -52,6 +175,7 @@ namespace TarziniYarat.UI.MVC.Areas.Admin.Controllers
             }
             ViewBag.Color = color;
         }
+
         private void GetAllCategories()
         {
             List<SelectListItem> categories = new List<SelectListItem>();
@@ -62,43 +186,14 @@ namespace TarziniYarat.UI.MVC.Areas.Admin.Controllers
             ViewBag.Categories = categories;
         }
 
-        //while creating a new product,we need to add product's brand which was created before.
-        private void GetAllBrands()
+        [HttpPost]
+        public JsonResult GetCategory(int id)
         {
-            List<SelectListItem> brands = new List<SelectListItem>();
-            foreach (Brand item in _brandService.GetAll())
-            {
-                brands.Add(new SelectListItem { Text = item.CompanyName, Value = item.BrandID.ToString() });
-            }
-            ViewBag.Brands = brands;
-        }
-        public ActionResult AddProduct()
-        {
-            GetBodyFromEnum();
-            GetColorFromEnum();
-            GetAllBrands();
-            GetAllCategories();
-            return View();
+            Category category = _categoryService.GetByID(id);
+
+            return Json(category, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult AddProduct(Product p)
-        {
-            GetBodyFromEnum();
-            GetColorFromEnum();
-            GetAllBrands();
-            GetAllCategories();
-
-            if (p.Description != null && p.ProductTitle != null) //buraya bak
-            {
-                _productService.Add(p);
-            }
-            return View();
-        }
-
-        public JsonResult DeleteProduct()
-        {
-            return Json(JsonRequestBehavior.AllowGet);
-        }
         public ActionResult CategoryList()
         {
             List<Category> categoryList = _categoryService.GetAll();
@@ -144,125 +239,78 @@ namespace TarziniYarat.UI.MVC.Areas.Admin.Controllers
             return View();
         }
 
-        public ActionResult UpdateCategory(int catID)
-        {
-            Category category = _categoryService.GetByID(catID);
-            return View(category);
-        }
-
+        //public ActionResult UpdateCategory(int catID)
+        //{
+        //    Category category = _categoryService.GetByID(catID);
+        //    return View(category);
+        //}
 
         [HttpPost]
-        public ActionResult UpdateCategory(Category _category)
+        public JsonResult UpdateCategory(Category model)
         {
             try
             {
-                Category category = _categoryService.GetByID(_category.CategoryID);
-                category.Description = _category.Description;
-                category.CategoryName = _category.CategoryName;
+                Category category = _categoryService.GetByID(model.CategoryID);
+                category.CategoryName = model.CategoryName;
+                category.Description = model.Description;
                 _categoryService.Update(category);
-                return RedirectToAction("CategoryList");
-
             }
             catch (Exception ex)
             {
-
-                throw new Exception("Güncelleme başarısız oldu.");
+                throw new Exception(ex.Message);
             }
+            return Json("ok", JsonRequestBehavior.AllowGet);
         }
+
+        //[HttpPost]
+        //public ActionResult UpdateCategory(Category _category)
+        //{
+        //    try
+        //    {
+        //        Category category = _categoryService.GetByID(_category.CategoryID);
+        //        category.Description = _category.Description;
+        //        category.CategoryName = _category.CategoryName;
+        //        _categoryService.Update(category);
+        //        return RedirectToAction("CategoryList");
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        throw new Exception("Güncelleme başarısız oldu.");
+        //    }
+        //}
+
         public JsonResult DeleteCategory()
         {
             return Json(JsonRequestBehavior.AllowGet);
 
         }
 
-        public ActionResult BrandList()
-        {
-            return View(_brandService.GetAll());
-        }
-
-        public ActionResult AddBrand()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult AddBrand(Brand brand)
-        {
-            _brandService.Add(brand);
-            return RedirectToAction("BrandList");
-        }
-        [HttpPost]
-        public JsonResult UpdateBrand(Brand brd)
-        {
-            try
-            {
-                Brand brand = _brandService.GetByID(brd.BrandID);
-                brand.CompanyName = brd.CompanyName;
-                _brandService.Update(brand);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            return Json("ok", JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpPost]
-        public JsonResult GetBrand(int id)
-        {
-            Brand brand = _brandService.GetByID(id);
-
-            return Json(brand, JsonRequestBehavior.AllowGet);
-
-        }
-        [HttpPost]
-        public JsonResult DeleteBrand(int id)
-        {
-            try
-            {
-                _brandService.Delete(id);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            return Json("ok", JsonRequestBehavior.AllowGet);
-        }
-
-
-        public ActionResult PersonList()
-        {
-            List<Person> personList = _personService.GetAll();
-
-            return View(personList);
-        }
-
-        public JsonResult AddPerson()
-        {
-            return Json(JsonRequestBehavior.AllowGet);
-        }
-        public JsonResult ActivatePerson(int personID)
-        {
-            Person person = _personService.GetByID(personID);
-            person.IsActive = true;
-            _personService.Update(person);
-            return Json("ok", JsonRequestBehavior.AllowGet);
-
-        }
-
-        public JsonResult DeletePerson()
-        {
-            return Json(JsonRequestBehavior.AllowGet);
-        }
         public ActionResult ShipperList()
         {
             List<Shipper> shippers = _shipperService.GetAll();
             return View(shippers);
         }
+
         public JsonResult DeleteShipper(int shipperID)
         {
             _shipperService.Delete(shipperID);
             return Json("ok", JsonRequestBehavior.AllowGet);
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
